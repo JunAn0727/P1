4f758da03e6cac2a6af3a8c68d7e372536d3f10a
JunAn0277 <ganja-wm17@student.tarc.edu.my>
2019-10-24 10:21:24 +0800

    P1 final version

--- app/src/main/java/com/example/p1/MainActivity.kt
+++ app/src/main/java/com/example/p1/MainActivity.kt
@@ -5,7 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +22,27 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        countup_button.setOnClickListener{ countUp() }
 
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+        val rt = arrayOf(resultText, resultText2, resultText3)
+        for(rst in rt) {
+            if (rst.text.toString().toIntOrNull() == null) {
+                rst.text = "1"
+            } else {
+                val r1 = rst.text.toString().toInt()
+                if (r1 < 6) {
+                    rst.text = (r1 + 1).toString()
+                } else {
+                    rst.text = r1.toString()
+                }
+            }
+        }
     }
 
     private fun rollDice() {
@@ -31,11 +58,11 @@ class MainActivity : AppCompatActivity() {
         resultText2.text = randomInt2.toString()
         resultText3.text = randomInt3.toString()
 
-        val sumButton: Button = findViewById(R.id.sum_button)
-        sumButton.setOnClickListener {
-            val sumV = randomInt + randomInt2 + randomInt3
-            val sumValue: TextView = findViewById(R.id.sumValue)
-            sumValue.text = sumV.toString()
+        val sumButton: Button = findViewById(R.id.reset_button)
+        reset_button.setOnClickListener {
+            resultText.text = "0"
+            resultText2.text = "0"
+            resultText3.text = "0"
         }
     }
 }
